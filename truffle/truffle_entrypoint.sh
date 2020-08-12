d566e3eeda33cc3aeb75ea6bedb8c5097d05a573
Naokiakazawa <naoki9akazawa@gmail.com>
2020-08-12 16:03:12 +0900

    rebuilding environment

--- truffle/truffle_entrypoint.sh
+++ truffle/truffle_entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-DIR="/usr/app/src/contracts"
+DIR="/usr/src/contracts"
 if [ ! -d $DIR ];then
-    cd /usr/app/src
+    cd /usr/src
     echo -e "\n===> creating a bare Truffle project\n"
     truffle init
+    echo -e "\n===> installing @truffle/hdwallet-provider\n"
+    npm init -y
+    npm install @truffle/hdwallet-provider
 fi
 /bin/bash
